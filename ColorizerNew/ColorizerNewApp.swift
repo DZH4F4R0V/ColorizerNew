d8e48123491fdad2d164fb34378c01bd72b212ba
DZH4F4R0V <jeff-122@ya.ru>
2023-06-13 22:56:40 +0300

    Initial Commit

---
+++ ColorizerNew/ColorizerNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizerNewApp.swift
+//  ColorizerNew
+//
+//  Created by J Eff on 13.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorizerNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
